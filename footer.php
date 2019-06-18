f0712cdc029f177747a328ade68248ec96da8045
Evin Dunn <evindunn14@gmail.com>
2019-06-18 08:08:46 -0700

    Adds scan-specific files

---
+++ footer.php
@@ -0,0 +1,21 @@
+</td>
+	</tr>
+	<tr>
+		<td class="scanfooter" colspan="3">
+	        <div style="width:90%;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;" >
+                <div style="width:100px;padding:0px 20px 0px 20px;display:table-cell;vertical-align: middle;">
+                    <a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=1207371"target="_blank"> <img src="<?php echo $clientRoot; ?>/images/layout/NSF_web_transperent.gif" width="100" height="100" alt="The National Science Foundation" /> </a>
+                </div>
+                <div style="padding-left:10px;display:table-cell;vertical-align: middle;">
+                        <b>This project made possible by <a href="http://www.nsf.gov/awardsearch/showAward.do?AwardNumber=1207371" target="_blank">National Science Foundation Award EF 1207371</a></b>
+                </div>
+                <div style="width:200px;padding:0px 20px 0px 20px;display:table-cell;vertical-align: middle;">
+                    <a href="https://www.idigbio.org/"target="_blank"> <img src="<?php echo $clientRoot; ?>/images/layout/idigbioLogo.png" style="width:200px;" alt="iDigBio" /> </a>
+                </div>
+                <div style="width:200px;padding:0px 20px 0px 20px;display:table-cell;vertical-align: middle;">
+                    <a href="https://fieldguide.net/figures"target="_blank"> <img src="<?php echo $clientRoot; ?>/images/layout/fg_logo.png" style="width:200px;" alt="FieldGuide" /> </$
+                </div>
+            </div>
+		</td>
+	</tr>
+</table>
