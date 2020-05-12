3322150b6c38a3f5c5d9907b6a5d196190aad33a
Evin Dunn <evindunn14@gmail.com>
2020-05-11 17:59:21 -0700

    fix twig template cacheing

--- config/twig.php
+++ config/twig.php
@@ -4,12 +4,15 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
+$cacheDir = "/tmp/.twig-cache";
 $loader = new FilesystemLoader(__DIR__ . "/../views");
 $options = [
   "strict_variables" => true,
   "debug" => false,
-//  "cache" => "/tmp/.twig-cache",
-  "cache" => false
+  "cache" => $cacheDir,
 ];
 
+if (!file_exists($cacheDir)) {
+    mkdir($cacheDir, 0700);
+}
 $twig = new Environment($loader, $options);
