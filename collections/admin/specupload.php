9505fadeb4b7fe31564802aed304e59b2f59fa12
Evin Dunn <evindunn14@gmail.com>
2021-01-13 09:51:17 -0700

    Remove upload limit cap

--- collections/admin/specupload.php
+++ collections/admin/specupload.php
@@ -206,7 +206,6 @@ $duManager->loadFieldMap();
 			<?php
 			$maxUpload = ini_get('upload_max_filesize');
 			$maxUpload = str_replace("M", "000000", $maxUpload);
-			if($maxUpload > 100000000) $maxUpload = 100000000;
 			echo 'var maxUpload = '.$maxUpload.";\n";
 			?>
 			var file = inputObj.files[0];
